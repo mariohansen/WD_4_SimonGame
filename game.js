a1cabd66d574abdc0743b8bbe7660c3eac94ed45
Mario Hansen <128413163+mariohansen@users.noreply.github.com>
2024-12-08 14:45:19 +0100

    added Eventhandlers and functions

--- game.js
+++ game.js
@@ -1,31 +1,53 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
-
-//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+var level = 0;
 var userClickedPattern = [];
 
-//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
-$(".btn").click(function() {
-
-  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-  var userChosenColour = $(this).attr("id");
-
-  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-  userClickedPattern.push(userChosenColour);
-
-  console.log(userClickedPattern);
-
+$(document).one("keydown", function() {
+    nextSequence();
+    $("h1").text("Level " + level);
 });
 
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    animatePress(userChosenColour); 
+    checkAnswer(userClickedPattern.length - 1);
+  });
+
 function nextSequence() {
+    userClickedPattern = [];
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    level++;
+    $("h1").text("Level " + level);
 
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+    }
 }
